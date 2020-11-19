0b277a605dfe1946c77eb9824948847f42d46255
Gabriel Cestaro <gabrielcestaro94@gmail.com>
2020-11-19 12:38:52 -0300

    salvando apenas a raiz

--- src/main/java/com/github/gcestaro/services/ArvoreFilhoAddStrategy.java
+++ src/main/java/com/github/gcestaro/services/ArvoreFilhoAddStrategy.java
@@ -4,8 +4,12 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 
 import com.github.gcestaro.models.Arvore;
@@ -21,45 +25,63 @@ public class ArvoreFilhoAddStrategy implements ArvoreAddStrategy {
 
 	private final ArvoreRepository repository;
 
+	private final MongoTemplate mongoTemplate;
+
 	@Override
 	public void add(Arvore arvore) {
 
-		List<Arvore> arvores = repository.findAll();
+		Optional<Arvore> raiz = buscarRaiz(arvore);
 
-		log.info("arvores - {}", arvores);
+		raiz.ifPresent(r -> {
+			log.info("raiz - {}", r);
 
+			addItem(arvore, r);
+
+			log.info("raiz alterada - {}", r);
+
+			repository.save(r);
+		});
+	}
+
+	private Optional<Arvore> buscarRaiz(Arvore arvore) {
+		Query query = new Query();
+
+		StringBuilder fieldBuilder = new StringBuilder("itens");
+
+		for (int i = 1; i < arvore.getLevel() - 2; i++) {
+			fieldBuilder.append(".itens");
+		}
+
+		fieldBuilder.append("._id");
+
+		query.addCriteria(Criteria.where(fieldBuilder.toString()).is(arvore.getPai()));
+
+		return Optional.ofNullable(mongoTemplate.findOne(query, Arvore.class));
+	}
+
+	private void addItem(Arvore item, Arvore raiz) {
+		List<Arvore> iterador = descerAteNivelPai(item, raiz);
+
+		log.info("iterador {}", iterador);
+
+		iterador.stream()
+				.filter(a -> a.getId() == item.getPai())
+				.findFirst()
+				.ifPresent(a -> a.addItem(item));
+	}
+
+	private List<Arvore> descerAteNivelPai(Arvore item, Arvore raiz) {
 		List<Arvore> iterador = new ArrayList<>();
-		iterador.addAll(arvores);
+		iterador.addAll(raiz.getItens());
 
-		addItem(arvore, iterador);
-
-		log.info("arvores - {}", arvores);
-
-		salvarRaiz(arvore, arvores);
-	}
-
-	private void salvarRaiz(Arvore arvore, List<Arvore> arvores) {
-
-		
-		
-		repository.saveAll(arvores);
-	}
-
-	private void addItem(Arvore arvore, List<Arvore> iterador) {
-		for (int i = 1; i < arvore.getLevel() - 1; i++) {
+		for (int i = 1; i < item.getLevel() - 2; i++) {
 			iterador = iterador.stream()
 					.map(Arvore::getItens)
 					.filter(Objects::nonNull)
 					.flatMap(Collection::stream)
 					.collect(Collectors.toList());
 		}
-
-		log.info("iterador {}", iterador);
-
-		iterador.stream()
-				.filter(a -> a.getId() == arvore.getPai())
-				.findFirst()
-				.ifPresent(a -> a.addItem(arvore));
+		return iterador;
 	}
 
 	@Override
